3b09c883341a8e58f14899443a48ff3b4e048894
kermiter <13717821254@163.com>
2018-11-13 18:18:24 +0800

    721

---
+++ leetcode/721.AccountsMerge/solution.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+class Solution{
+    public:
+        vector<vector<string>> accountsMerge(vector<vector<string>>& accounts)
+        {
+            vector<vector<string> >res;
+            if(accounts.empty())
+                return res;
+            map<string,vector<int> >mtmp;
+
+        
+        }
+
+
+
+};
+int main()
+{
+
+
+}
